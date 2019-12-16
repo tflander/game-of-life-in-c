57b02d6484d5eff4a63eced15d20f67169c7df78
Todd Flanders <todd.a.flanders@accenture.com>
2019-12-15 20:06:39 -0500

    start solving for C

--- src/rules.c
+++ src/rules.c
@@ -1,6 +1,37 @@
 #include "rules.h"
+#include <stdio.h>
 
 bool cell_alive(bool alive_now, int neighbors)
 {
-    return true;
+    if (alive_now)
+        return neighbors > 1 && neighbors < 4;
+    else 
+        return neighbors == 3;
+}
+
+int count_neighbors(int col, int row) {
+    return 0;
+}
+
+void print(char* arr, int numCols, int numRows) 
+{ 
+    int c, r; 
+    for (c = 0; c < numCols; c++) {
+      for (r = 0; r < numRows; r++) {
+        char x = *((arr+c*numRows) + r);
+        if (x == ' ') {
+            x = '.';
+        }
+        printf("%c ", x); 
+      }
+      printf("\n");
+    }
+} 
+
+void wipeGrid(char* data, int numCols, int numRows) {
+    for(int c = 0; c < numCols; ++c) {
+        for(int r = 0; r < numRows; ++r) {
+            *((data+c*numRows) + r) = ' ';
+        }
+    }
 }
--- test/test_rules.c
+++ test/test_rules.c
@@ -3,10 +3,32 @@
 #include "rules.h"
 
 TEST_GROUP(Rules);
+TEST_GROUP(Neighbors);
+TEST_GROUP(Grid);
 
 TEST_SETUP(Rules) {}
-
 TEST_TEAR_DOWN(Rules) {}
+TEST_SETUP(Neighbors) {}
+TEST_TEAR_DOWN(Neighbors) {}
+TEST_SETUP(Grid) {}
+TEST_TEAR_DOWN(Grid) {}
+
+TEST(Grid, create_empty_grid) {
+    int numCols = 4;
+    int numRows = 3;
+    char gridData[numCols][numRows];
+    wipeGrid((char*)gridData, numCols, numRows);
+
+    for(int c = 0; c < numCols; ++c) {
+       for(int r = 0; r < numRows; ++r) {
+           TEST_ASSERT_EQUAL(' ', gridData[c][r]);
+       }
+    }
+}
+
+TEST(Neighbors, has_one_neighbor) {
+   TEST_ASSERT_EQUAL(1, count_neighbors(0,0));
+}
 
 TEST(Rules, live_cell_with_fewer_than_two_neighbors_dies)
 {
@@ -14,7 +36,38 @@ TEST(Rules, live_cell_with_fewer_than_two_neighbors_dies)
     TEST_ASSERT_FALSE(cell_alive(true, 0));
 }
 
+TEST(Rules, live_cell_with_two_or_three_neighbors_survives)
+{
+    TEST_ASSERT_TRUE(cell_alive(true, 2));
+    TEST_ASSERT_TRUE(cell_alive(true, 3));
+}
+
+TEST(Rules, live_cell_with_more_than_three_neighbors_dies)
+{
+    TEST_ASSERT_FALSE(cell_alive(true, 4));
+    TEST_ASSERT_FALSE(cell_alive(true, 5));
+}
+
+TEST(Rules, dead_cell_with_three_neighbors_becomes_live)
+{
+    TEST_ASSERT_TRUE(cell_alive(false, 3));
+}
+
+TEST(Rules, dead_cell_with_two_neighbors_stays_dead)
+{
+    TEST_ASSERT_FALSE(cell_alive(false, 2));
+}
+
 TEST_GROUP_RUNNER(Rules)
 {
     RUN_TEST_CASE(Rules, live_cell_with_fewer_than_two_neighbors_dies);
+    RUN_TEST_CASE(Rules, live_cell_with_two_or_three_neighbors_survives);
+    RUN_TEST_CASE(Rules, live_cell_with_more_than_three_neighbors_dies);
+    RUN_TEST_CASE(Rules, dead_cell_with_three_neighbors_becomes_live);
+    RUN_TEST_CASE(Rules, dead_cell_with_two_neighbors_stays_dead);
+
+    RUN_TEST_CASE(Grid, create_empty_grid);
+    // TODO: not ready
+    // RUN_TEST_CASE(Neighbors, has_one_neighbor);
+    
 }
