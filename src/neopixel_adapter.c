0516b8aec3b65d7c049989470f4b1cfabc1fb3c2
Todd Flanders <todd.a.flanders@accenture.com>
2019-12-21 20:59:27 -0500

    finish neopixel adapter

--- src/neopixel_adapter.c
+++ src/neopixel_adapter.c
@@ -1,5 +1,14 @@
 #include "neopixel_adapter.h"
+#include <stdio.h>
 
 int strandOffsetForPoint(struct Grid gridForAdapter, struct Point gridPoint) {
-    return 0;
+
+    bool isEvenRow = (gridPoint.y % 2) == 0;
+
+    int offset = gridPoint.x;
+    if(!isEvenRow) {
+        offset = gridForAdapter.numCols - offset - 1;
+    }
+
+    return offset + (gridPoint.y * gridForAdapter.numCols);
 }
--- test/test_neopixel_adapter.c
+++ test/test_neopixel_adapter.c
@@ -20,9 +20,47 @@ TEST(NeoPixelAdapter, point_0_0_is_neopixel_0)
     TEST_ASSERT_EQUAL(0, strandOffsetForPoint(gridForAdapter, (struct Point){.x=0, .y=0}));
 }
 
+TEST(NeoPixelAdapter, point_1_0_is_neopixel_1)
+{
+    TEST_ASSERT_EQUAL(1, strandOffsetForPoint(gridForAdapter, (struct Point){.x=1, .y=0}));
+}
+
+TEST(NeoPixelAdapter, point_4_0_is_neopixel_4)
+{
+    TEST_ASSERT_EQUAL(4, strandOffsetForPoint(gridForAdapter, (struct Point){.x=4, .y=0}));
+}
+
+TEST(NeoPixelAdapter, point_4_1_is_neopixel_5)
+{
+    TEST_ASSERT_EQUAL(5, strandOffsetForPoint(gridForAdapter, (struct Point){.x=4, .y=1}));
+}
+
+TEST(NeoPixelAdapter, point_3_1_is_neopixel_6)
+{
+    TEST_ASSERT_EQUAL(6, strandOffsetForPoint(gridForAdapter, (struct Point){.x=3, .y=1}));
+}
+
+TEST(NeoPixelAdapter, point_0_1_is_neopixel_9)
+{
+    TEST_ASSERT_EQUAL(9, strandOffsetForPoint(gridForAdapter, (struct Point){.x=0, .y=1}));
+}
+
+TEST(NeoPixelAdapter, point_0_2_is_neopixel_10)
+{
+    TEST_ASSERT_EQUAL(10, strandOffsetForPoint(gridForAdapter, (struct Point){.x=0, .y=2}));
+}
+
 TEST_GROUP_RUNNER(NeoPixelAdapter)
 {
     RUN_TEST_CASE(NeoPixelAdapter, point_0_0_is_neopixel_0);
+    RUN_TEST_CASE(NeoPixelAdapter, point_1_0_is_neopixel_1);
+    RUN_TEST_CASE(NeoPixelAdapter, point_4_1_is_neopixel_5);
+    RUN_TEST_CASE(NeoPixelAdapter, point_3_1_is_neopixel_6);
+    
+    RUN_TEST_CASE(NeoPixelAdapter, point_0_1_is_neopixel_9);
+
+    RUN_TEST_CASE(NeoPixelAdapter, point_4_0_is_neopixel_4);
+    RUN_TEST_CASE(NeoPixelAdapter, point_0_2_is_neopixel_10);
 }
 
 
