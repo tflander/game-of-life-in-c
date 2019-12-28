5b4c4b51e263dced162a818705a0dadf40d118c1
Todd Flanders <todd.a.flanders@accenture.com>
2019-12-28 13:43:33 -0500

    finish redoux

--- test/test_main.c
+++ test/test_main.c
@@ -4,7 +4,7 @@ static void run_all_tests(void) {
     RUN_TEST_GROUP(Rules); 
     RUN_TEST_GROUP(Grid); 
     // RUN_TEST_GROUP(Oscillator); 
-    // RUN_TEST_GROUP(NeoPixelAdapter); 
+    RUN_TEST_GROUP(NeoPixelAdapter); 
 }
 
 int main(int argc, const char *argv[])
